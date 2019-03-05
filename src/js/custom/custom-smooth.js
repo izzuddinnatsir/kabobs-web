198487e159fda5d18149e7b62511bb572f1d2bf6
izznatsir <natsirizzuddin@gmail.com>
2019-03-05 18:18:45 +0700

    Finished all pages, barbajs is not yet implemented

---
+++ src/js/custom/custom-smooth.js
@@ -0,0 +1,73 @@
+import Smooth from 'smooth-scrolling'
+const isMobile = require('ismobilejs')
+
+class Parallax extends Smooth {
+
+  constructor(opt) {
+    super(opt)
+    this.createExtraBound()
+    this.resizing = false
+    this.cache = null
+    this.dom.divs = Array.prototype.slice.call(opt.divs, 0)
+    this.dom.tabnav = opt.tabnav
+  }
+
+  createExtraBound() {
+    ['getCache', 'inViewport', 'resizeOnClick']
+      .forEach((fn) => this[fn] = this[fn].bind(this))
+  }
+
+  getCache() {
+    this.cache = []
+    this.dom.divs.forEach((el, index) => {
+      el.style.display = 'block'
+      el.style.transform = 'none'
+      const scrollY = this.vars.target
+      const bounding = el.getBoundingClientRect()
+      const bounds = {
+        el: el,
+        state: true,
+        top: bounding.top + scrollY,
+        left: bounding.left,
+        center: bounding.height / 2,
+        bottom: bounding.bottom + scrollY,
+        speed: isMobile.tablet ? el.getAttribute('data-speed-tablet') || '-1' : isMobile.phone ? el.getAttribute('data-speed-phone') || '-1' : el.getAttribute('data-speed') || '-1',
+        direction: el.getAttribute('data-direction') || null
+      }
+      if (index === 4) {
+        console.log(bounding.top, scrollY, bounds.top)
+      }
+      // this.vars.bounding = bounding.bottom > this.vars.bounding ? bounding.bottom - window.innerHeight : this.vars.bounding;
+      this.cache.push(bounds)
+    })
+    // get bounding value based on the container (.vs-section) height
+    this.vars.bounding = this.dom.section.getBoundingClientRect().height - (this.vars.native ? 0 : this.vars.height)
+  }
+
+  run() {
+    this.dom.divs.forEach(this.inViewport)
+    this.dom.section.style[this.prefix] = this.getTransform(this.vars.current * -1)
+    super.run()
+  }
+
+  inViewport(el, index) {
+    if (!this.cache || this.resizing) return
+    const cache = this.cache[index]
+    const current = this.vars.current
+    const transformAsTrigger = (cache.top - current) * cache.speed
+    const transform = cache.direction === 'top' ? -((cache.top - (current + this.vars.height)) * cache.speed) : (cache.top - (current + this.vars.height)) * cache.speed
+    const top = Math.round((cache.top - this.vars.height + transformAsTrigger) - current)
+    const bottom = Math.round((cache.bottom + transform) - current)
+    const inview = bottom > 0 && top < 0
+    if (inview) {
+      // el.style.border = '2px solid green'
+      // el.style.display = 'block'
+      el.style[this.prefix] = this.getTransform(transform)
+    } else {
+      // add red border if out of viewport
+      // el.style.border = '2px solid red'
+    }
+  }
+}
+
+export default Smooth
--- src/js/script.js
+++ src/js/script.js
@@ -1,15 +1,124 @@
-import '../css/style.scss'
 import isMobile from 'ismobilejs'
-import ParallaxSmooth from './custom/custom-smooth-scrolling'
-import GSAP from 'gsap'
+import Smooth from 'smooth-scrolling'
+import ParallaxSmooth from './custom/custom-smooth-parallax'
+import { TweenLite, CSSRulePlugin, TweenMax, TimelineLite, Power1, TimelineMax } from 'gsap'
+import Swiper from 'swiper'
+import ResizeSensor from 'css-element-queries/src/ResizeSensor'
 
-const scroller = new ParallaxSmooth({
+const html = document.documentElement
+const viewport = document.querySelector('#viewport')
+const navTriggerBtn = document.querySelector('.topbar__navtrigger')
+const navScreen = document.querySelector('.mainNav')
+const navCloseBtn = document.querySelector('.mainNav__header__closeBtn')
+const floatingKebab = document.querySelectorAll('.hero__kebab')
+let request
+
+const parallax = new ParallaxSmooth({
   native: false,
   direction: 'vertical',
+  noscrollbar: isMobile.tablet && isMobile.phone ? true : false,
   listener: document.querySelector('.vs-viewport'),
   section: document.querySelector('.vs-section'),
   divs: document.querySelectorAll('*[data-speed]'),
-  speed: isMobile.phone ? 0.15 : 0.075
+  ease: isMobile.phone ? 0.06 : 0.075
 })
 
-scroller.init();
+const scroller = new Smooth({
+  native: isMobile.tablet && isMobile.phone ? true : false,
+  noscrollbar: isMobile.tablet && isMobile.phone ? true : false,
+  direction: 'vertical',
+  listener: document.querySelector('.vs-viewport'),
+  section: document.querySelector('.vs-section')
+})
+
+var swiper = new Swiper(".swiper-container", {
+  spaceBetween: 50,
+  slidesPerView: 1,
+  navigation: {
+    nextEl: ".menuSlideNext",
+    prevEl: ".menuSlidePrev"
+  },
+  pagination: {
+    el: ".swiper-pagination"
+  },
+  hashNavigation: {
+    replaceState: true,
+    watchState: true
+  },
+  loop: true,
+  effect: "fade"
+});
+
+initEventListeners()
+
+function initEventListeners() {
+  if (html.classList.contains('html-index')) {
+    TweenLite.set(floatingKebab[0], { transform: 'translate3d(0, 6px, 0)' });
+    TweenLite.set(floatingKebab[1], { transform: 'translate3d(0, -6px, 0)' });
+    document.addEventListener('DOMContentLoad', indexPreloader)
+    window.onload = indexOnLoad
+  } else if (!html.classList.contains('html-index') && !html.classList.contains('html-menu') && !html.classList.contains('html-menu-slider')) {
+    scroller.init();
+    const repeatResize = new ResizeSensor(scroller.dom.section, function () {
+      scroller.resize();
+    })
+    requestAnimationFrame(changeTriggerColor);
+  } else if (html.classList.contains('html-menu')) {
+    scroller.init()
+    const repeatResize = new ResizeSensor(scroller.dom.section, function () {
+      scroller.resize();
+    })
+  }
+
+
+  navTriggerBtn.addEventListener('click', revealNav)
+  navCloseBtn.addEventListener('click', hideNav)
+}
+
+function indexPreloader(e) {
+
+}
+
+function indexOnLoad(e) {
+  const indexResize = new ResizeSensor(parallax.dom.section, function () {
+    parallax.resize();
+  })
+
+  parallax.init();
+
+  TweenMax.to(floatingKebab[0], 1, { transform: ('translate3d(0, -6px, 0)'), ease: Power1.easeInOut, yoyo: true, repeat: -1 });
+  TweenMax.to(floatingKebab[1], 1, { transform: ('translate3d(0, 6px, 0)'), ease: Power1.easeInOut, yoyo: true, repeat: -1 });
+}
+
+function revealNav(e) {
+  const circle = document.querySelector('.navtrigger__circle')
+  const bars = 'navbar__trigger__rowbar'
+
+  e.target === navTriggerBtn || e.target === circle || e.target.classList.contains(bars) ? startTween() : null
+
+  function startTween() {
+    TweenLite.set(navScreen, { display: 'flex' });
+    TweenMax.to(navScreen, 0.5, { transform: 'translate3d(0, 0, 0)', ease: Power1.easeOut });
+  }
+}
+
+function hideNav(e) {
+  const icon = e.target === document.querySelector('.mainNav__header__closeBtn img')
+  const parent = e.target === navCloseBtn
+
+  icon || parent ? startTween() : null
+
+  function startTween() {
+    TweenMax.to(navScreen, 0.4, { transform: 'translate3d(0, 100vh, 0)', ease: Power1.easeOut });
+    TweenMax.to(navScreen, 0, { display: 'none', delay: 0.4 });
+  }
+}
+
+function changeTriggerColor() {
+  const pageHeader = document.querySelector('section.section--header').getBoundingClientRect().height
+
+  request = requestAnimationFrame(changeTriggerColor)
+
+  scroller.vars.current > pageHeader ? navTriggerBtn.classList.add('topbar__navtrigger--black') : navTriggerBtn.classList.remove('topbar__navtrigger--black');
+}
+
